ebadae4366f952c655e308c72d3967b883d08bfc
lchisolm1217 <laquanachisolm@ac.c4q.nyc>
2018-01-16 21:32:18 -0500

    added part01

---
+++ app/src/main/java/com/example/quana/practicalmidtermassessment/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.quana.practicalmidtermassessment;
+
+/**
+ * Created by Quana on 1/16/18.
+ */
+
+public class LoginActivity {
+}
--- app/src/main/java/com/example/quana/practicalmidtermassessment/LoopActivity.java
+++ app/src/main/java/com/example/quana/practicalmidtermassessment/LoopActivity.java
@@ -1,15 +1,58 @@
 package com.example.quana.practicalmidtermassessment;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new MyTask().execute(0);
 
 
     }
+
+    private class MyTask extends AsyncTask<Integer, Integer, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            final TextView text1 = (TextView) findViewById(R.id.text1);
+            text1.setText("Loops Completed" + "doInBackground() ");
+            Intent intent = new Intent(this, LoginActivity.class);
+            EditText editText = (EditText) findViewById(R.id.editText);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            final TextView text1 = (TextView) findViewById(R.id.text1);
+            text1.setText("Loops Completed" + "doInBackground() ");
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+            int i;
+            for (i = 0; i <= 100000; i++) ; {
+
+                publishProgress(i);
+
+                return null;
+            }
+
+        }
+    }
+
 }
+
